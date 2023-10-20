e88109cb51e73b0a9ad91353621699578c56700e
Claudia Villarroel <claudia.villarroel@live.com>
2023-10-20 15:47:17 -0300

    :lipstick: Ajustes nav y home

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,5 +1,15 @@
+import './Home.css'
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="container-fluid" id="home">
+      <h1>
+        Bienvenido a <b>Happy Cake</b>
+      </h1>
+      <h5>El lugar de los pasteles felices</h5>
+      <img src="../../../../src/assets/img/happyCake.png" height="200px" />
+    </div>
+  );
 };
 
 export default Home;
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/desafio1-react2-happycake/",
+  base:"/desafio1_react2_happycake/",
   plugins: [react()],
 })
